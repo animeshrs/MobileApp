217c6a1c93d2bb813f9e2e5c48484b0e09da7912
animeshrs <william.animesh@gmail.com>
2020-02-04 22:07:09 +0000

    Add project files.

---
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+            Slider.Value = 0.5;
+        }
+    }
+}
